2c5cc514b8e71e8aed4f385c29bbb93aae46fb28
willjuniorpp <willjrpp@gmail.com>
2014-11-28 14:19:48 -0200

    New Commit

---
+++ Vitrini/Vitrini/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vitrini
+//
+//  Created by Willian Pinho on 11/28/14.
+//  Copyright (c) 2014 Willian Pinho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
