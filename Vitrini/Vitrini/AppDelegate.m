6275168ea47af7d34dd4a6a5f07ffd0ceca1a7d1
riheldo <riheldomelo@hotmail.com>
2014-12-11 15:07:21 -0200

    COMENTARIO

--- Vitrini/Vitrini/AppDelegate.m
+++ Vitrini/Vitrini/AppDelegate.m
@@ -14,6 +14,9 @@
 
 @implementation AppDelegate
 
+//
+// COMENTARIO RIHELDO
+//
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
